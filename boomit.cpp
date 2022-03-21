8a7b9ae45b60fcf59513b47e798d01510d922c06
Kapra <211240081@smail.nju.edu.cn>
2022-03-21 20:46:54 +0800

    发现了超好用的函数gotoxy，把闪屏和光标解决掉了！

--- boomit.cpp
+++ boomit.cpp
@@ -1,47 +1,69 @@
 #include <bits/stdc++.h>
-#include<conio.h>
+#include <windows.h>
+#include <conio.h>
 #define ffor(i, a, n) for (long long i = a; i <= n; i++)
 using namespace std;
-int cnt,jym=1,h=20;
-int boomap[1000][1000],mx=3,my=3;
-void deal_input()
-{
+int cnt, jym = 1, h = 20;
+int boomap[1000][1000], mx = 3, my = 3;
+
+
+
+void deal_input() {
 	char ch;
-	if(_kbhit())
-	{
-		ch=_getch();
-		if(ch=='w'||ch=='H')my--;
-		if(ch=='d'||ch=='M')mx++;
-		if(ch=='a'||ch=='K')mx--;
-		if(ch=='s'||ch=='P')my++;
-		int k=int(ch);if(k==10||k==32)return;
-	}return;
+	if (_kbhit()) {
+		ch = _getch();
+		if (ch == 'w' || ch == 'H')
+			my--;
+		if (ch == 'd' || ch == 'M')
+			mx++;
+		if (ch == 'a' || ch == 'K')
+			mx--;
+		if (ch == 's' || ch == 'P')
+			my++;
+		int k = int(ch);
+		if (k == 10 || k == 32)
+			return;
+	}
+	return;
 }
-void display()
-{
-	system("cls");
-	ffor(i,1,h)
-	{
-		ffor(j,1,h)
-		{
-			if(mx==j&&my==i)cout<<"@";
-			else if(i==1||i==h||j==1||j==h)cout<<"#";
-			else cout<<" ";
+
+
+
+void display() {
+	gotoxy(0, 0);
+	ffor(i, 1, h) {
+		ffor(j, 1, 2 * h) {
+			if (mx == j && my == i)
+				cout << "s";
+			else if (i == 1 || i == h || j == 1 || j == 2 * h)
+				cout << "#";
+			else
+				cout << " ";
 		}
-		cout<<endl;
+		cout << "\n";
 	}
 }
-int main()
-{
-	/*while (1)					
+
+
+
+void init() {
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	GetConsoleCursorInfo(hOut, &cci);
+	cci.bVisible = FALSE;
+	SetConsoleCursorInfo(hOut, &cci);
+}
+
+
+int main() {
+	/*while (1)
 	{
-		Show();					
-		InputWithData();		
-		InputWithoutData();		
+		Show();
+		InputWithData();
+		InputWithoutData();
 		system("cls");			}*/
-	//init();
-	while(1)
-	{
+	init();
+	while (1) {
 		display();
 		deal_input();
 		/*cnt++;
@@ -50,6 +72,7 @@ int main()
 			deal_time();
 			cnt=0;
 		}*/
+		cnt = 0;
 	}
 	return 0;
 }
