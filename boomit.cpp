d4013bc98f63fd2ea8d9ca536f40a5cdaf5df85a
Kapra <211240081@smail.nju.edu.cn>
2022-03-21 21:52:21 +0800

    解决了刷屏问题

--- boomit.cpp
+++ boomit.cpp
@@ -3,29 +3,65 @@
 #include <conio.h>
 #define ffor(i, a, n) for (long long i = a; i <= n; i++)
 using namespace std;
-int cnt, jym = 1, h = 20;
-int boomap[1000][1000], mx = 3, my = 3;
+int cnt, jym = 1, h = 20, turn = 1;
+int boomap[1000][1000], mx = 3, my = 3, yx = h - 3, yy = h - 3;
 
 
 
-void deal_input() {
+
+
+
+
+
+void deal_input1() {
+	if (turn == 2)
+		return;
+	turn = 2;
 	char ch;
 	if (_kbhit()) {
 		ch = _getch();
-		if (ch == 'w' || ch == 'H')
+		if (ch == 'w')
 			my--;
-		if (ch == 'd' || ch == 'M')
+		else if (ch == 'd')
 			mx++;
-		if (ch == 'a' || ch == 'K')
+		else if (ch == 'a')
 			mx--;
-		if (ch == 's' || ch == 'P')
+		else if (ch == 's')
 			my++;
 		int k = int(ch);
-		if (k == 10 || k == 32)
+		if (k == 10)
 			return;
 	}
 	return;
 }
+
+
+void deal_input2() {
+	if (turn == 1)
+		return;
+	turn = 1;
+	char ch;
+	if (_kbhit()) {
+		ch = _getch();
+		if (ch == 'H')
+			yy--;
+		else if (ch == 'M')
+			yx++;
+		else if (ch == 'K')
+			yx--;
+		else if (ch == 'P')
+			yy++;
+		int k = int(ch);
+		if (k == 32)
+			return;
+	}
+	return;
+}
+
+
+
+
+
 
 
 
@@ -34,7 +70,9 @@ void display() {
 	ffor(i, 1, h) {
 		ffor(j, 1, 2 * h) {
 			if (mx == j && my == i)
-				cout << "s";
+				cout << "@";
+			else if (yx == j && yy == i)
+				cout << "&";
 			else if (i == 1 || i == h || j == 1 || j == 2 * h)
 				cout << "#";
 			else
@@ -43,6 +81,11 @@ void display() {
 		cout << "\n";
 	}
 }
+
+
+
+
+
 
 
 
@@ -55,6 +98,11 @@ void init() {
 }
 
 
+
+
+
+
+
 int main() {
 	/*while (1)
 	{
@@ -65,7 +113,8 @@ int main() {
 	init();
 	while (1) {
 		display();
-		deal_input();
+		deal_input1();
+		deal_input2();
 		/*cnt++;
 		if(cnt==jym)
 		{
